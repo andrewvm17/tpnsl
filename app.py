84aeebe11b54565b0e7c3a9d7b6e87e7e3d2b005
andrewvm17 <andrewtce10@gmail.com>
2023-06-12 17:03:03 -0400

    connected firebase

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("tpnsl-c0752-firebase-adminsdk-gstnb-2caeeab7f4.json")
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 @app.route('/')
