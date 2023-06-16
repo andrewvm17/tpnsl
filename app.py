0ff31351a099ddd3f5fc2ae04a0f1f8763be35af
andrewvm17 <andrewtce10@gmail.com>
2023-06-16 19:40:17 -0400

    added standings

--- app.py
+++ app.py
@@ -16,22 +16,98 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bç5\xdc\xe3*\x1eZ\x10\xd0\x96\x85\xa9\x15\xce<\x84\xfa'
 
-
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     form = MatchReportForm()
     if form.validate_on_submit():
-        # Save the data to Firebase
-        doc_ref = db.collection('match_reports').document()
-        doc_ref.set({
-            'home_team': form.home_team.data,
-            'home_team_score': form.home_team_score.data,
-            'away_team': form.away_team.data,
-            'away_team_score': form.away_team_score.data
-        })
-        flash('Match report for {} vs. {} submitted'.format(form.home_team.data, form.away_team.data))
-        return redirect(url_for('index'))
+        
+        home_team = form.home_team.data
+        print(home_team)
+        away_team = form.away_team.data
+        home_team_score = int(form.home_team_score.data)
+        away_team_score = int(form.away_team_score.data)
+        division = form.division.data
+        season = 'Spring2023'  # Manually set season
+
+        # Assume that db is your initialized Firestore client
+        collection_ref = db.collection('seasons').document(season).collection('divisions').document(division).collection('standings')
+
+        # Get all documents in the collection
+        docs = collection_ref.stream()
+
+        # Iterate through each document
+        for doc in docs:
+            doc_data = doc.to_dict()
+            
+            # If 'wins' field does not exist, add it
+            if 'wins' not in doc_data:
+                doc_data['wins'] = 0
+            
+            # Do the same for 'losses' and 'draws' fields
+            if 'losses' not in doc_data:
+                doc_data['losses'] = 0
+            if 'draws' not in doc_data:
+                doc_data['draws'] = 0
+
+            if 'points' not in doc_data:
+                doc_data['points'] = 0
+
+            # Update the document with the new fields
+            collection_ref.document(doc.id).set(doc_data)
+
+        # Determine result
+        if home_team_score > away_team_score:
+            home_result = 'win'
+            away_result = 'loss'
+        elif home_team_score < away_team_score:
+            home_result = 'loss'
+            away_result = 'win'
+        else:
+            home_result = away_result = 'draw'
+
+        match_report_data = {
+            'home_team': home_team,
+            'home_team_score': home_team_score,
+            'home_result': home_result,
+            'away_team': away_team,
+            'away_team_score': away_team_score,
+            'away_result': away_result,
+            'division': division,
+        }
+        
+        # Save match report to match_reports sub-collection of the correct division
+        match_report_ref = db.collection('seasons').document(season).collection('divisions').document(division).collection('match_reports').document()
+        match_report_ref.set(match_report_data)
+
+        # Update standings
+        # Assumes that each team has a document in the standings collection with the same name as the team
+        standings_ref = db.collection('seasons').document(season).collection('divisions').document(division).collection('standings')
+        home_team_standings = standings_ref.document(home_team).get().to_dict()
+        away_team_standings = standings_ref.document(away_team).get().to_dict()
+
+        # Update standings based on result
+        if home_result == 'win':
+            home_team_standings['wins'] += 1
+            home_team_standings['points'] += 3
+            away_team_standings['losses'] += 1
+        elif home_result == 'loss':
+            home_team_standings['losses'] += 1
+            away_team_standings['wins'] += 1
+            away_team_standings['points'] += 3
+        else:
+            home_team_standings['draws'] += 1
+            home_team_standings['points'] += 1
+            away_team_standings['draws'] += 1
+            away_team_standings['points'] += 1
+
+        # Save updated standings back to database
+        standings_ref.document(home_team).set(home_team_standings)
+        standings_ref.document(away_team).set(away_team_standings)
+
+        return redirect(url_for('admin'))
     return render_template('admin.html', title='Report Match', form=form)
+
+
 
 @app.route("/registration", methods=['GET', 'POST'])
 def registration():
@@ -49,6 +125,14 @@ def registration():
         return redirect(url_for('home'))
     return render_template('registration.html', title='Register', form=form)
 
+@app.route('/standings', methods=['GET'])
+def standings():
+    season = 'Spring2023'  # Manually set season
+    division = 'coed10'  # Replace with your division name
+    standings_ref = db.collection('seasons').document(season).collection('divisions').document(division).collection('standings')
+    standings = [{'name': doc.id, **doc.to_dict()} for doc in standings_ref.stream()]
+    standings.sort(key=lambda x: x['points'], reverse=True)
+    return render_template('standings.html', title='Standings', standings=standings)
 
 
 @app.route('/')
--- forms.py
+++ forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, DateField, IntegerField, SelectFie
 from wtforms.validators import DataRequired, Email
 
 class RegistrationForm(FlaskForm):
+    
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     school = StringField('School', validators=[DataRequired()])
@@ -11,9 +12,10 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
 class MatchReportForm(FlaskForm):
-    home_team = SelectField('Home Team', choices=[('lime green', 'Lime Green'), ('forest green', 'Forest Green'), ('red', 'Red'), ('gold', 'Gold')], validators=[DataRequired()])
+    division = SelectField('Division', choices=[('coed10', 'CoEd 10')])
+    home_team = SelectField('Home Team', choices=[('lime_green', 'Lime Green'), ('black', 'Black'), ('purple', 'Purple'),('forest_green', 'Forest Green'), ('red', 'Red'), ('gold', 'Gold')], validators=[DataRequired()])
     home_team_score = IntegerField('Home Team Score', validators=[DataRequired()])
-    away_team = SelectField('Away Team', choices=[('lime green', 'Lime Green'), ('forest green', 'Forest Green'), ('red', 'Red'), ('gold', 'Gold')], validators=[DataRequired()])
+    away_team = SelectField('Away Team', choices=[('lime_green', 'Lime Green'), ('black', 'Black'), ('purple', 'Purple'),('forest_green', 'Forest Green'), ('red', 'Red'), ('gold', 'Gold')], validators=[DataRequired()])
     away_team_score = IntegerField('Away Team Score', validators=[DataRequired()])
     date = DateField('Match Date', format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField('Submit Report')
